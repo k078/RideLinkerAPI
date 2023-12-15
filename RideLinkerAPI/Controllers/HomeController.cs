b3a9dace598cdabe5b9138e82ed9c9c39bdb2764
k078 <77967048+k078@users.noreply.github.com>
2023-12-15 11:45:36 +0100

    Add project files.

---
+++ RideLinkerAPI/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace rideLinkerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index() aangeroepen");
+            return Ok("Home controller!");
+        }
+    }
+}
