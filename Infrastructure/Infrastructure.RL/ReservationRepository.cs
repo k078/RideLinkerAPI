e3f8787ae36e47fec02eeb0ef5add279d12ffbd1
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-12-18 14:43:27 +0100

    Create ReservationRepository.cs

---
+++ Infrastructure/Infrastructure.RL/ReservationRepository.cs
@@ -0,0 +1,47 @@
+﻿using Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.RL
+{
+    public class ReservationRepository
+    {
+        private readonly YourDbContext _context;
+
+        public ReservationRepository(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Reservation> GetByIdAsync(int id)
+        {
+            return await _context.Reservations.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Reservation>> GetAllAsync()
+        {
+            return await _context.Reservations.ToListAsync();
+        }
+
+        public async Task AddAsync(Reservation reservation)
+        {
+            await _context.Reservations.AddAsync(reservation);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Reservation reservation)
+        {
+            _context.Reservations.Update(reservation);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Reservation reservation)
+        {
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
