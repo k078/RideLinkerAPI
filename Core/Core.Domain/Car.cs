a9073cb400e3f2334b2393649531fe36b83145f9
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-12-18 14:42:58 +0100

    Create Car.cs

---
+++ Core/Core.Domain/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public string? Image { get; set; }
+        public int LocationId { get; set; }
+        public bool Available { get; set; }
+        public Location? Location { get; set; }
+        public ICollection<Trip> Trips { get; set; } = new List<Trip>();
+
+    }
+}
