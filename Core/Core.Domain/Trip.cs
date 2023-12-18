9532998facbbed99ef669eea7d80cf0a1bfb1e7e
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-12-18 14:43:05 +0100

    Create Trip.cs

---
+++ Core/Core.Domain/Trip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string? Departure { get; set; }
+        public string? Destination { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int DriverId { get; set; }
+        public int CarId { get; set; }
+        public Car? Car { get; set; }
+        public User? Driver { get; set; }
+        public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+
+    }
+}
