067453c99849dba172cf6d92d74e780bae2bc3ba
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-12-18 14:43:01 +0100

    Create Location.cs

---
+++ Core/Core.Domain/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public ICollection<Car> Cars { get; set; } = new List<Car>();
+        public User? User { get; set; }
+        public Trip? Trip { get; set; }
+
+    }
+}
