a5f35b69f15ed0ce15f749eba3aca891dfdaaca8
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-12-18 14:43:26 +0100

    Create LocationRepository.cs

---
+++ Infrastructure/Infrastructure.RL/LocationRepository.cs
@@ -0,0 +1,48 @@
+﻿using Core.Domain;
+using Core.DomainService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.RL
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly YourDbContext _context;
+
+        public LocationRepository(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Location> GetByIdAsync(int id)
+        {
+            return await _context.Locations.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Location>> GetAllAsync()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+
+        public async Task AddAsync(Location location)
+        {
+            await _context.Locations.AddAsync(location);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Location location)
+        {
+            _context.Locations.Update(location);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Location location)
+        {
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
