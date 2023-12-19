476941cfa6d0676a2d5144b327862d4722c8baf0
stenreij <74412704+stenreij@users.noreply.github.com>
2023-12-19 14:53:09 +0100

    TRIP CRUD

---
+++ Core/Core.DomainService/Services/TripService.cs
@@ -0,0 +1,45 @@
+﻿using Core.Domain;
+using Core.DomainService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DomainService.Services
+{
+    public class TripService : ITripService
+    {
+        private readonly ITripRepository _repo;
+
+        public TripService(ITripRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+        public Task<IEnumerable<Trip>> GetAllAsync()
+        {
+            return _repo.GetAllAsync();
+        }
+
+        public Task<Trip> GetByIdAsync(int id)
+        {
+            return _repo.GetByIdAsync(id);
+        }
+        public Task AddAsync(Trip trip)
+        {
+            return _repo.AddAsync(trip);
+        }
+
+        public Task UpdateAsync(Trip trip)
+        {
+            return _repo.UpdateAsync(trip);
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            return _repo.DeleteAsync(id);
+        }
+    }
+}
---
+++ RideLinkerAPI/Controllers/TripController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Core.Domain;
+using Core.DomainService.Interfaces;
+
+namespace RideLinkerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TripController : ControllerBase
+    {
+        private readonly ILogger<CarController> _logger;
+        private readonly ITripService _tripService;
+
+        public TripController(ILogger<CarController> logger, ITripService tripService)
+        {
+            _logger = logger;
+            _tripService = tripService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllTrips()
+        {
+            _logger.LogInformation("GetAllTrips() aangeroepen");
+
+            try
+            {
+                var trips = await _tripService.GetAllAsync();
+                return Ok(trips);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Fout bij het ophalen van trips: {ex.Message}");
+                return StatusCode(500, "Er is een interne fout opgetreden bij het ophalen van trips.");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTripById(int id)
+        {
+            _logger.LogInformation($"GetTripById({id}) aangeroepen");
+            var trip = await _tripService.GetByIdAsync(id);
+
+            if (trip == null)
+            {
+                _logger.LogError($"Trip met id {id} niet gevonden");
+                return NotFound();
+            }
+
+            return Ok(trip);
+        }
+
+
+        [HttpPost()]
+        public async Task<IActionResult> AddTrip([FromBody] Trip inTrip)
+        {
+            _logger.LogInformation("AddTrip() aangeroepen");
+
+            try
+            {
+                await _tripService.AddAsync(inTrip);
+                return Ok(inTrip);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Fout bij het toevoegen van een trip: {ex.Message}");
+                return StatusCode(500, "Er is een interne fout opgetreden bij het toevoegen van een trip.");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTrip(int id, [FromBody] Trip updatedTrip)
+        {
+            _logger.LogInformation($"UpdateTrip({id}) aangeroepen");
+
+            if (updatedTrip == null)
+            {
+                return BadRequest();
+            }
+
+            updatedTrip.Id = id;
+
+            try
+            {
+                await _tripService.UpdateAsync(updatedTrip);
+
+                if (updatedTrip == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(updatedTrip);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Fout bij het bijwerken van trip: {ex.Message}");
+                return StatusCode(500, "Er is een interne fout opgetreden bij het bijwerken van een trip.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTrip(int id)
+        {
+            _logger.LogInformation($"DeleteTrip({id}) aangeroepen");
+
+            try
+            {
+                await _tripService.DeleteAsync(id);
+                return Ok("Trip ID gedelete: " + id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Fout bij het verwijderen van de trip: {ex.Message}");
+                return StatusCode(500, "Er is een interne fout opgetreden bij het verwijderen van de trip.");
+            }
+        }
+    }
+}
